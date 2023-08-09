22c2db6d89e7ed055357aeaed5acc7066f245c76
tadin-project <takumiinui24@gmail.com>
2023-08-09 21:34:30 +0700

    setup theme done

---
+++ lib/presentation/widget/app_input_text.dart
@@ -0,0 +1,37 @@
+import 'package:booking_app/config/config.dart';
+import 'package:flutter/material.dart';
+
+class AppInputText extends StatefulWidget {
+  const AppInputText({Key? key, this.controller, required this.label})
+      : super(key: key);
+  final TextEditingController? controller;
+  final String label;
+
+  @override
+  State<AppInputText> createState() => _AppInputTextState();
+}
+
+class _AppInputTextState extends State<AppInputText> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: AppDimen.h40,
+      child: TextFormField(
+        controller: widget.controller,
+        decoration: InputDecoration(
+          hintText: widget.label,
+          hintStyle: AppFont.componentMedium,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: AppDimen.w16),
+        ),
+      ),
+    );
+  }
+}
